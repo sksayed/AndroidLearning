34584fa434453063be60628331033282f5ede047
Sheikh Sayed <sksayed@live.com>
2019-10-17 17:17:57 +0600

    gradle thik hoise kintu main method likha hoi nai

--- app/src/main/java/lab/itsoul/com/dailygoods/app/sayedandroidlearning/NavigationDashboard.java
+++ app/src/main/java/lab/itsoul/com/dailygoods/app/sayedandroidlearning/NavigationDashboard.java
@@ -1,14 +1,18 @@
 package lab.itsoul.com.dailygoods.app.sayedandroidlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class NavigationDashboard extends AppCompatActivity {
 
+   private DrawerLayout myDrawer ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_dashboard);
+        this.myDrawer = findViewById(R.id.navbar_drawlayout_id);
     }
 }
